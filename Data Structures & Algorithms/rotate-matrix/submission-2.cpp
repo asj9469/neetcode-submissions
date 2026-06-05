7ffd47833b799b8ff41adb185072b44761c76b42
Anson <79544046+asj9469@users.noreply.github.com>
2026-06-04 17:13:09 -0700

    Add: rotate-matrix - submission-2

---
+++ Data Structures & Algorithms/rotate-matrix/submission-2.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        
+        // int n = matrix.size();
+        // int l = 0, r = n - 1;
+
+        // while (l < r){
+
+        //     for (int i = 0; i < (r-l); i++){
+        //         int top = l, bottom = r;
+        //         int topLeft = matrix[top][l+i];
+
+        //         // replace the top left with the bottom left
+        //         matrix[top][l+i] = matrix[bottom-i][l];
+        //         matrix[bottom-i][l] = matrix[bottom][r-i];
+        //         matrix[bottom][r-i] = matrix[top+i][r];
+        //         matrix[top+i][r] = topLeft;
+        //     }
+
+        //     l += 1;
+        //     r -= 1;
+        // }
+
+        int n = matrix.size();
+        
+        reverse(matrix.begin(), matrix.end());
+        for (int i = 0; i < n; i++){
+            for (int j = i+1; j < n; j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        
+        return;
+    }
+};
